c1fb12554126c9b950d27bd06ff85644a4b7c42f
IcePlayer974 <julien.delplanque.jd@gmail.com>
2026-01-09 11:58:21 +0100

    add_dependaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp) // Ajout du plugin KSP pour Room
 }
 
 android {
     namespace = "fr.delplanque.tp_androidstudio"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36 // Utilise 35 pour la stabilité.
 
     defaultConfig {
         applicationId = "fr.delplanque.tp_androidstudio"
-        minSdk = 26
+        minSdk = 26 // Couvre >85% des utilisateurs [cite: 35]
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -33,8 +35,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+
+        }
     }
     buildFeatures {
         compose = true
@@ -50,6 +55,25 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // --- NOUVEAUX AJOUTS ---
+
+    // Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // Retrofit & Gson (API)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+    // Coil (Images)
+    implementation(libs.coil.compose)
+
+    // Room (Base de données)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler) // Utilisation de ksp pour le processeur d'annotations
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
