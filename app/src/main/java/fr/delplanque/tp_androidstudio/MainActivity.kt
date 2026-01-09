d4fdfb134c8b3d2ea63614096db38e2ea71ea61a
IcePlayer974 <julien.delplanque.jd@gmail.com>
2026-01-09 12:18:25 +0100

    test_connecter

--- app/src/main/java/fr/delplanque/tp_androidstudio/MainActivity.kt
+++ app/src/main/java/fr/delplanque/tp_androidstudio/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!, Salut FDP",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
