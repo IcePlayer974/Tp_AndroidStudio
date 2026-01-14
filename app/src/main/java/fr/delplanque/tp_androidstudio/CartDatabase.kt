919d7519521e99a0446c115bc92e32d98c2a2689
IcePlayer974 <julien.delplanque.jd@gmail.com>
2026-01-14 11:25:20 +0100

    safe_branch_for_cart

--- app/src/main/java/fr/delplanque/tp_androidstudio/CartDatabase.kt
+++ app/src/main/java/fr/delplanque/tp_androidstudio/CartDatabase.kt
@@ -2,22 +2,4 @@ package fr.delplanque.tp_androidstudio
 
 import androidx.room.*
 
-@Entity(tableName = "cart_items")data class CartEntity(
-    @PrimaryKey val id: Int,
-    val title: String,
-    val price: Double,
-    val image: String,
-    val quantity: Int
-)
 
-@Dao
-interface CartDao {
-    @Query("SELECT * FROM cart_items")
-    suspend fun getAll(): List<CartEntity>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(item: CartEntity)
-
-    @Query("DELETE FROM cart_items WHERE id = :id")
-    suspend fun delete(id: Int)
-}
